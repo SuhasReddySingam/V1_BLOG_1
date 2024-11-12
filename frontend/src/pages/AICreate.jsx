0f31bb96bb15b4530c34cdc7f09f3367ffb8a7dc
SuhasReddySingam <s.suhasreddy2006@gmail.com>
2024-11-12 13:30:03 +0530

    Update AICreate.jsx

--- frontend/src/pages/AICreate.jsx
+++ frontend/src/pages/AICreate.jsx
@@ -6,7 +6,7 @@ import { motion } from "framer-motion";
 import { useAuthStore } from "../store/authStore";
 import {HfInference} from '@huggingface/inference';
 const CreatePage = () => {
-    const inference=new HfInference("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");
+    const inference=new HfInference("Enter APU Key");
     const model="meta-llama/Llama-3.2-3B-Instruct";
     const { user }=useAuthStore();
     const [newPrompt,setNewPrompt]=useState("");
